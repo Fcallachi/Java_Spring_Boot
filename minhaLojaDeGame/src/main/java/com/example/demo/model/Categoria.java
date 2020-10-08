1be7ce00d029c768bedaf870c0dc5bf269f05a61
unknown <fernandocallachi@gmail.com>
2020-10-08 19:34:49 -0300

    versao1

--- minhaLojaDeGame/src/main/java/com/example/demo/model/Categoria.java
+++ minhaLojaDeGame/src/main/java/com/example/demo/model/Categoria.java
@@ -1,14 +1,12 @@
 package com.example.demo.model;
 
-import java.util.Date;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import javax.persistence.Temporal; 
-import javax.persistence.TemporalType;
 import javax.validation.constraints.Size;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.sun.istack.NotNull; 
@@ -34,8 +32,8 @@ public class Categoria {
 	@Size(min = 10, max = 500)
 	private String console;	
 	
-	/*@ManyToOne 
+	@ManyToOne 
 	@JsonIgnoreProperties("categoria")
-	private Categoria categoria;*/
+	private Produto produto;
 		
 }
